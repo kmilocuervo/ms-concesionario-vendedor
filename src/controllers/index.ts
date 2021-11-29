9790fdc27384a0aca720aafbab44f5f08053f517
Kmilo <KmiloCrow27@gmail.com>
2021-11-28 19:40:54 -0500

    se agrega controladores

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,5 @@ export * from './ping.controller';
 export * from './cliente-vendedor.controller';
 export * from './vendedor-cliente.controller';
 export * from './vendedor-usuario-vendedor.controller';
+export * from './cliente.controller';
+export * from './vendedor.controller';
